b6692e91ec27ecfca88971cc09a60bbf7639c1e8
johannes <johannes.muljana@ait.nsw.edu.au>
2019-12-11 14:21:48 +1100

    first commit added firebase config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,21 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [
     BrowserModule,
     IonicModule.forRoot(),
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp( environment.firebase ),
+    AngularFireAuthModule,
+    AngularFirestoreModule
   ],
   providers: [
     StatusBar,
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDo9Ld7HuIJCEWXC5l0Pwnwmajb1pElF5Q",
+    authDomain: "notes-demo-d8c46.firebaseapp.com",
+    databaseURL: "https://notes-demo-d8c46.firebaseio.com",
+    projectId: "notes-demo-d8c46",
+    storageBucket: "notes-demo-d8c46.appspot.com",
+    messagingSenderId: "138020715402",
+    appId: "1:138020715402:web:744698db457c30e510e673",
+    measurementId: "G-CZG99TWSME"
+  }
 };
 
 /*
