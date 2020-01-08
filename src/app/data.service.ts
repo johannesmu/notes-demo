fcd2284fc029896bb64966d8ca1fa7928b7c59aa
johannes <jmuljana@gmail.com>
2020-01-08 12:07:12 +1100

    added observable data in data service

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Note } from '../models/note.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
+  private notesCollection: AngularFirestoreCollection<Note>;
+  notes: Observable<Note[]>;
 
-  constructor() { }
+  constructor( private afs: AngularFirestore ) { 
+    this.notesCollection = afs.collection<Note> ('notes');
+    this.notes = this.notesCollection.snapshotChanges().pipe(
+      map(actions => actions.map(a => {
+        const data = a.payload.doc.data() as Note;
+        const id = a.payload.doc.id;
+        return { id, ...data };
+      }))
+    );
+  }
 }
